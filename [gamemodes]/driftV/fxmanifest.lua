55c7b55d185bff0bc947cc943e872e610ace2ef0
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-01-11 14:21:25 +0100

    Reworked drift counter in NUI

--- [gamemodes]/driftV/fxmanifest.lua
+++ [gamemodes]/driftV/fxmanifest.lua
@@ -1,32 +1,41 @@
------------------------ [ MenuV ] -----------------------
--- GitHub: https://github.com/ThymonA/menuv/
--- License: GNU General Public License v3.0
---          https://choosealicense.com/licenses/gpl-3.0/
--- Author: Thymon Arens <contact@arens.io>
--- Name: MenuV
--- Version: 1.4
--- Description: FiveM menu library for creating menu's
------------------------ [ MenuV ] -----------------------
-fx_version 'cerulean'
-game 'gta5'
-lua54 'yes'
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+ui_page "client/web/hud.html"
+
+files {
+    "client/web/hud.html",
+    "client/web/js/gauge.min.js",
+    "client/web/js/hud.js",
+    "client/web/css/hud.css",
+    "client/web/fonts/Oswald-Light.eot",
+    "client/web/fonts/Oswald-Light.svg",
+    "client/web/fonts/Oswald-Light.ttf",
+    "client/web/fonts/Oswald-Light.woff",
+    "client/web/fonts/Oswald-Light.woff2",
+    "client/web/fonts/Oswald-Regular.eot",
+    "client/web/fonts/Oswald-Regular.svg",
+    "client/web/fonts/Oswald-Regular.ttf",
+    "client/web/fonts/Oswald-Regular.woff",
+    "client/web/fonts/Oswald-Regular.woff2",
+    "client/web/images/speedcircle.png"
+}
 
 client_scripts {
-
-    'client/RageUI/RMenu.lua',
-    'client/RageUI/menu/RageUI.lua',
-    'client/RageUI/menu/Menu.lua',
-    'client/RageUI/menu/MenuController.lua',
-    'client/RageUI/components/*.lua',
-    'client/RageUI/menu/elements/*.lua',
-    'client/RageUI/menu/items/*.lua',
-    'client/RageUI/menu/panels/*.lua',
-    'client/RageUI/menu/windows/*.lua',
-
+    "client/RageUI/RMenu.lua",
+    "client/RageUI/menu/RageUI.lua",
+    "client/RageUI/menu/Menu.lua",
+    "client/RageUI/menu/MenuController.lua",
+    "client/RageUI/components/*.lua",
+    "client/RageUI/menu/elements/*.lua",
+    "client/RageUI/menu/items/*.lua",
+    "client/RageUI/menu/panels/*.lua",
+    "client/RageUI/menu/windows/*.lua",
     "client/**/*.lua"
 }
 
 server_scripts {
     "server/class/*.lua",
-    "server/*.lua",
+    "server/*.lua"
 }
