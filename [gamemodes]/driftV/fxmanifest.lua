fc4e9105160e5c688297aa899a75d8cf7db65841
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-01-14 10:33:26 +0100

    Fixing RageUi double load

--- [gamemodes]/driftV/fxmanifest.lua
+++ [gamemodes]/driftV/fxmanifest.lua
@@ -22,15 +22,15 @@ files {
 }
 
 client_scripts {
-    "client/RageUI/RMenu.lua",
-    "client/RageUI/menu/RageUI.lua",
-    "client/RageUI/menu/Menu.lua",
-    "client/RageUI/menu/MenuController.lua",
-    "client/RageUI/components/*.lua",
-    "client/RageUI/menu/elements/*.lua",
-    "client/RageUI/menu/items/*.lua",
-    "client/RageUI/menu/panels/*.lua",
-    "client/RageUI/menu/windows/*.lua",
+    "RageUI/RMenu.lua",
+    "RageUI/menu/RageUI.lua",
+    "RageUI/menu/Menu.lua",
+    "RageUI/menu/MenuController.lua",
+    "RageUI/components/*.lua",
+    "RageUI/menu/elements/*.lua",
+    "RageUI/menu/items/*.lua",
+    "RageUI/menu/panels/*.lua",
+    "RageUI/menu/windows/*.lua",
     "client/**/*.lua"
 }
 
