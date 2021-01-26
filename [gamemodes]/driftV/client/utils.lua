854611b514e8d975d04ab0b83fc4d8ea66442b8a
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-01-26 11:52:37 +0100

    Update utils.lua

--- [gamemodes]/driftV/client/utils.lua
+++ [gamemodes]/driftV/client/utils.lua
@@ -468,3 +468,42 @@ function Trim(value)
 		return nil
 	end
 end
+
+function GetClosestPlayer()
+	local pPed = GetPlayerPed(-1)
+	local players = GetActivePlayers()
+	local coords = GetEntityCoords(pPed)
+	local pCloset = nil
+	local pClosetPos = nil
+	local pClosetDst = nil
+	for k,v in pairs(players) do
+		if GetPlayerPed(v) ~= pPed then
+			local oPed = GetPlayerPed(v)
+			local oCoords = GetEntityCoords(oPed)
+			local dst = GetDistanceBetweenCoords(oCoords, coords, true)
+			if pCloset == nil then
+				pCloset = v
+				pClosetPos = oCoords
+				pClosetDst = dst
+			else
+				if dst < pClosetDst then
+					pCloset = v
+					pClosetPos = oCoords
+					pClosetDst = dst
+				end
+			end
+		end
+	end
+
+	return pCloset, pClosetDst
+end
+
+function DisplayClosetPlayer()
+	local pPed = GetPlayerPed(-1)
+	local pCoords = GetEntityCoords(pPed)
+	local pCloset = GetClosestPlayer(pCoords)
+	if pCloset ~= -1 then
+		local cCoords = GetEntityCoords(GetPlayerPed(pCloset))
+		DrawMarker(20, cCoords.x, cCoords.y, cCoords.z+1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.3, 0.3, 0.3, 0, 255, 0, 170, 0, 1, 2, 0, nil, nil, 0)
+	end
+end
