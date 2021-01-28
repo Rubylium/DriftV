39148801998d8a6a79409429654b3b1c00eda3a8
Rubylium <alexisoko0@gmail.com>
2021-01-28 16:19:42 +0100

    New little succes notificaiton

--- [gamemodes]/driftV/client/gameplay/success.lua
+++ [gamemodes]/driftV/client/gameplay/success.lua
@@ -84,3 +84,97 @@ function CheckDriftPoints(points)
         return false
     end
 end
+
+
+local alpha = 0
+local subAlpha = 0
+local display = false
+function DisplayLittleSucces(title)
+    p:SetSucces(title)
+    local baseX = 0.5 -- gauche / droite ( plus grand = droite )
+    local baseY = 1.00 -- Hauteur ( Plus petit = plus haut )
+    local baseWidth = 0.0 -- Longueur
+    local baseHeight = 0.03 -- Epaisseur
+
+    local long = string.len(title)
+    for i = 1, long do
+        baseWidth = baseWidth + 0.006
+    end
+
+
+    Citizen.CreateThread(function()
+        for i = 0,255 do
+            alpha = alpha + 3
+            subAlpha = subAlpha + 3
+
+            if alpha >= 255 then
+                alpha = 255
+            end
+            if subAlpha >= 200 then
+                subAlpha = 200
+            end
+            Wait(0)
+        end
+        Wait(500)
+        for i = 0,255 do
+            alpha = alpha - 3
+            if alpha <= 0 then
+                alpha = 0
+            end
+            if subAlpha <= 0 then
+                subAlpha = 0
+            end
+            if alpha <= 200 then
+                subAlpha = subAlpha - 3
+            end
+            Wait(0)
+        end
+        display = false
+        alpha = 0
+        subAlpha = 0
+    end)
+
+    Citizen.CreateThread(function()
+        while display do
+            DrawRect(baseX, baseY - 0.060, baseWidth, baseHeight - 0.025, 110, 255, 168, alpha) -- Liseret
+            DrawRect(baseX, baseY - 0.043, baseWidth, baseHeight, 0, 0, 0, subAlpha) -- Bannière
+            DrawTexts(baseX, baseY - (0.043) - 0.013, title, true, 0.35, {255, 255, 255, alpha}, 2, 0) -- title
+            Wait(1)
+        end
+    end)
+end
+
+-- local baseX = 0.5 -- gauche / droite ( plus grand = droite )
+-- local baseY = 0.95 -- Hauteur ( Plus petit = plus haut )
+-- local baseWidth = 0.05 -- Longueur
+-- local baseHeight = 0.03 -- Epaisseur
+
+
+-- Citizen.CreateThread(function()
+--     while true do
+--         DrawRect(baseX, baseY - 0.060, baseWidth, baseHeight - 0.025, 110, 255, 168, 255) -- Liseret
+--         DrawRect(baseX, baseY - 0.043, baseWidth, baseHeight, 0, 0, 0, 200) -- Bannière
+--         DrawTexts(baseX, baseY - (0.043) - 0.013, "test", true, 0.35, {255, 255, 255, 255}, 2, 0) -- title
+--         Wait(1)
+--     end
+-- end)
+
+local LittleSucces = {}
+
+function AddLittleSucces(title)
+    table.insert(LittleSucces, {text = title})
+end
+
+Citizen.CreateThread(function()
+    while true do
+        if not display then
+            if LittleSucces[1] ~= nil then
+                display = true
+                DisplayLittleSucces(LittleSucces[1].text)
+                table.remove(LittleSucces, 1)
+                Wait(1000)
+            end
+        end
+        Wait(200)
+    end
+end)
