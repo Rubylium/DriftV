c67160a7300b1b9482981de378edeceddaaf1e7e
Rubylium <alexisoko0@gmail.com>
2021-01-30 15:04:42 +0100

    Force lower render lod

--- [gamemodes]/driftV/client/playerStuff/commands.lua
+++ [gamemodes]/driftV/client/playerStuff/commands.lua
@@ -25,7 +25,7 @@ RegisterCommand("record", function(source, args, rawCommand)
     end
 end, false)
 
-local render = 1.0
+local render = 0.7
 local renderToggle = false
 RegisterCommand("render", function(source, args, rawCommand)
     if args[1] ~= nil then
@@ -33,10 +33,9 @@ RegisterCommand("render", function(source, args, rawCommand)
             if args[2] == "true" then -- Maybe you could change that to on / off ? Maybe more userfriendly, it's string anyway so ...
                 renderToggle = true
                 render = tonumber(args[1])
-                loopRender()
             else
                 renderToggle = false
-                render = 1.0
+                render = 0.7
             end
         else
             -- Notification to tell the player he forgot the toggle value
@@ -46,17 +45,15 @@ RegisterCommand("render", function(source, args, rawCommand)
     end
 end, false)
 
-function loopRender() -- Thread in a function so it don't run at all when not called :D
-    Citizen.CreateThread(function()
-        while renderToggle do
-            Citizen.InvokeNative(0xA76359FC80B2438E, tonumber(render)) -- Ugly Native yeah, it don't have a name yet so we need to Invoke it
-            Wait(1) -- Native need to be called every frame tho, the game will reset the custom render if not
-        end
-    end)
-end
+Citizen.CreateThread(function()
+    while true do
+        Citizen.InvokeNative(0xA76359FC80B2438E, tonumber(render)) -- Ugly Native yeah, it don't have a name yet so we need to Invoke it
+        Wait(1) -- Native need to be called every frame tho, the game will reset the custom render if not
+    end
+end)
+
 
 TriggerEvent('chat:addSuggestion', '/render', 'Force a render distance, may help for performance and invisible map issue', {
     { name="scale", help="Distance between 0.1 and 1.0" },
     { name="toggle", help="true/false" }
 })
-
