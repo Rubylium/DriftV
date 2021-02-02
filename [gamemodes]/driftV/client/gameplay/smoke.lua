09ce14d2e9bceeee70e396d65ba50a3377e47cf8
Rubylium <alexisoko0@gmail.com>
2021-02-02 09:37:35 +0100

    Sync smoke with players

--- [gamemodes]/driftV/client/gameplay/smoke.lua
+++ [gamemodes]/driftV/client/gameplay/smoke.lua
@@ -1,4 +1,5 @@
 
+local vehiclesToCheck = {}
 local _SIZE = 0.2
 local _DENS = 10
 
@@ -6,65 +7,8 @@ local _SMOKE_ON = true
 
 local bone_list = {"wheel_lr","wheel_rr"}
 
-Citizen.CreateThread(function()
 
-    local base = "scr_recartheft"
-    local base2 = "core"
-
-    Request(base)
-    Request(base2)
-
-    while true do Citizen.Wait(0)
-
-        local ped = GetPlayerPed(-1)
-        local car = GetVehiclePedIsUsing(ped)
-        local ang,speed = Angle(car)
-
-
-        if _SMOKE_ON then
-            if IsPedInAnyVehicle(GetPlayerPed(-1), false) then
-                if speed * 3.6 >= 40 and ang >= 10 then
-                    local densBySpeed = (_SIZE * p:speed()) / 100
-                    local densByAngle = densBySpeed + (ang / 1000)
-                    DriftSmoke(base,"scr_wheel_burnout", car, _DENS, densByAngle)
-                end
-            end
-        end
-    end
-end)
-
---[[===============]]
---[[=== F U N C ===]]
---[[===============]]
-function Request(name)
-    RequestNamedPtfxAsset(name)
-    while not HasNamedPtfxAssetLoaded(name) do
-        Wait(1)
-    end
-end
-
-function DriftSmoke(base, sub, car, dens, size)
-    local all_part = {}
-
-    for i = 0, dens do
-        UseParticleFxAssetNextCall(base)
-        W1 = StartParticleFxLoopedOnEntityBone(sub, car, 0.05, 0, 0, 0, 0, 0, GetEntityBoneIndexByName(car, bone_list[1]), size, 0, 0, 0)
-
-        UseParticleFxAssetNextCall(base)
-        W2 = StartParticleFxLoopedOnEntityBone(sub, car, 0.05, 0, 0, 0, 0, 0, GetEntityBoneIndexByName(car, bone_list[2]), size, 0, 0, 0)
-
-        table.insert(all_part, 1, W1)
-        table.insert(all_part, 2, W2)
-    end
-
-    Citizen.Wait(1000)
-
-    for _,W1 in pairs(all_part) do
-        StopParticleFxLooped(W1, true)
-    end
-end
-
-function Angle(veh)
+local function Angle(veh)
     if not veh then return false end
     local vx,vy,_ = table.unpack(GetEntityVelocity(veh))
     local modV = math.sqrt(vx*vx + vy*vy)
@@ -78,4 +22,81 @@ function Angle(veh)
     local cosX = (sn*vx + cs*vy)/modV
     if cosX > 0.966 or cosX < 0 then return 0,modV end
     return math.deg(math.acos(cosX))*0.5, modV
+end
+
+Citizen.CreateThread(function()
+    while true do
+        for k,v in pairs(GetVehicles()) do
+            if GetEntitySpeed(v) * 3.6 >= 30 then
+                vehiclesToCheck[v] = v
+            else
+                vehiclesToCheck[v] = nil
+            end
+        end
+        Wait(500)
+    end
+end)
+
+Citizen.CreateThread(function()
+
+    local base = "scr_recartheft"
+    local base2 = "core"
+
+    Request(base)
+    Request(base2)
+
+    while true do 
+        Citizen.Wait(0)
+
+        local didCheck = false
+        for k,v in pairs(vehiclesToCheck) do
+            local car = v
+            local ang,speed = Angle(car)
+
+            
+
+            if _SMOKE_ON then
+                if speed * 3.6 >= 40 and ang >= 10 then
+                    didCheck = true
+                    local speed = speed * 3.6
+                    local densBySpeed = (_SIZE * speed) / 100
+                    local densByAngle = densBySpeed + (ang / 1000)
+                    DriftSmoke(base,"scr_wheel_burnout", car, _DENS, densByAngle)
+                end
+            end
+        end
+
+        if not didCheck then
+            Wait(500)
+        end
+    end
+end)
+
+--[[===============]]
+--[[=== F U N C ===]]
+--[[===============]]
+
+function Request(name)
+    RequestNamedPtfxAsset(name)
+    while not HasNamedPtfxAssetLoaded(name) do
+        Wait(1)
+    end
+end
+
+function DriftSmoke(base, sub, car, dens, size)
+    local all_part = {}
+
+    for i = 0, dens do
+        for k,v in pairs(bone_list) do
+            UseParticleFxAssetNextCall(base)
+            local W1 = StartParticleFxLoopedOnEntityBone(sub, car, 0.05, 0, 0, 0, 0, 0, GetEntityBoneIndexByName(car, v), size, 0, 0, 0)
+            table.insert(all_part, W1)
+        end
+    end
+
+    Citizen.Wait(1000)
+
+    for _,W1 in pairs(all_part) do
+        StopParticleFxLooped(W1, true)
+    end
 end
