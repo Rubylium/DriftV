836b13a9c970050efbf24e6a133d0c0e11788103
Rubylium <19718604+Rubylium@users.noreply.github.com>
2021-02-04 16:03:57 +0100

    Update loading screen

--- [gamemodes]/loading/__resource.lua
+++ [gamemodes]/loading/__resource.lua
@@ -1,30 +1,12 @@
-description 'A simple loading screen!'
+description "A simple loading screen!"
 
 files {
-    'index.html',
-
-    'css/index.css',
-    'css/colors.css',
-    'css/icomoon.css',
-    
-    'fonts/gravity.otf',
-    'fonts/icomoon.ttf',
-    
-    'img/cursor.png',    
-    'img/logo.png',
-    'img/bg1.jpg',
-    'img/bg2.jpg',
-    'img/bg3.jpg',
-    
-    'js/synn.js',
-    'js/config.js',
-    'js/logger.js',
-    'js/progressbar-handler.js',
-    'js/progressbar-main.js',
-    'js/progressbar-renderer.js',
-    'js/music-handler.js',
-    'js/music-controls.js',
-    'js/background-handler.js',
+    "index.html",
+    "css/*.css",
+    "fonts/gravity.otf",
+    "fonts/icomoon.ttf",
+    "img/*.png",
+    "js/*.js"
 }
 
-loadscreen 'index.html'
+loadscreen "index.html"
