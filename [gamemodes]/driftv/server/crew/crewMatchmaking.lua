9ac4e726622615a27b116b03a90fb1816a072609
Rubylium <alexisoko0@gmail.com>
2021-05-17 13:06:01 +0200

    Add UUID generator

--- [gamemodes]/driftv/server/crew/crewMatchmaking.lua
+++ [gamemodes]/driftv/server/crew/crewMatchmaking.lua
@@ -345,3 +345,11 @@ AddEventHandler("crew:CrewCarsAddScore", function(warID, crew, score, driftScore
     wars[warID].done = wars[warID].done + 1
     wars[warID].refresh()
 end)
+
+function uuid()
+    local template ='xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return string.gsub(template, '[xy]', function (c)
+        local v = (c == 'x') and math.random(0, 0xf) or math.random(8, 0xb)
+        return string.format('%x', v)
+    end)
+end
