c69cf45bf480b08b7d136043def4d5cdf8ef90b0
Rubylium <alexisoko0@gmail.com>
2021-05-22 10:49:27 +0200

    Force resync #1

--- [gamemodes]/driftv/server/commands.lua
+++
@@ -1,9 +0,0 @@
-RegisterCommand("give", function(source, args, rawCommand)
-    if source == 0 then
-        source = tonumber(args[1])
-        money = tonumber(args[2])
-        player[source].money = math.floor(player[source].money + money)
-        RefreshPlayerData(source)
-        player[source].needSave = true
-    end
-end, false)
